c862ef44683f1968b83760e1a8552abf027ac92b
sachin0110 <sachinhatvalne01@gmail.com>
2019-10-15 11:48:43 +0000

    Integrating first web api for getting usages

--- Frontend/src/app/components/apply-loan/steps-components/loan-details/loan-details.component.ts
+++ Frontend/src/app/components/apply-loan/steps-components/loan-details/loan-details.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { FormService } from '../form-service';
+import { SharedService } from 'src/app/services/shared.service';
 
+interface Usage {
+  id: number,
+  name: string
+}
 @Component({
   selector: 'app-loan-details',
   templateUrl: './loan-details.component.html',
@@ -10,20 +15,33 @@ import { FormService } from '../form-service';
 export class LoanDetailsComponent implements OnInit {
 
   step: FormGroup
-
+  otherUsage: string = '';
+  usageList: Array<Usage> = [];
   constructor(
     private _formBuilder: FormBuilder,
-    private formService: FormService
+    private formService: FormService,
+    private sharedService: SharedService
   ) {
     this.step = this._formBuilder.group({
       amount: ['', Validators.required],
       term: ['', Validators.required],
       usage: ['', Validators.required]
     });
-    this.formService.stepReady(this.step, 'two')
+    this.formService.stepReady(this.step, 'two');
   }
 
   ngOnInit() {
+    this.sharedService.getUsages().subscribe(usages => {
+      debugger;
+      this.usageList = usages;
+      // this.usageList.push({ id: 2, name: 'Debt Restructuring' });
+      // this.usageList.push({ id: 3, name: 'Other (Specify)' });
+    }, error => {
+
+    });
+  }
+  nextClicked() {
+debugger;
   }
 
 }
--- Frontend/src/app/services/shared.service.ts
+++ Frontend/src/app/services/shared.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SharedService {
-
-  constructor() { }
+  baseUrl: string = 'https://virtserver.swaggerhub.com/PrashantJaiswal17/MyTestAccount/1.0.0';
+  constructor(private http: HttpClient) { }
+  getUsages(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/usage`);
+  }
 }
