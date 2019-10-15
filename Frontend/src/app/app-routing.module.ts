5686c14be4d7ffc6e7e8474c70a68013b866953f
Swapnil Srivastava <56465793+swapnilsrivastava68@users.noreply.github.com>
2019-10-15 08:00:18 +0000

    adding basic routting to the app

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { MainNavComponent } from './components/main-nav/main-nav.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: MainNavComponent},
+  { path: 'home', component: DashboardComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
