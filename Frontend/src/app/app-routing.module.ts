b4e70f3c9ac4dfc1de8351a2277c4e7ae55cd3bf
sachin0110 <sachinhatvalne01@gmail.com>
2019-10-15 08:22:40 +0000

    removing ommented lines

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,11 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-import { MainNavComponent } from './components/main-nav/main-nav.component';
-// import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoanStatusComponent } from './components/loan-status/loan-status.component';
 import { ApplyLoanComponent } from './components/apply-loan/apply-loan.component';
-
 
 const routes: Routes = [
   { path: '', component: ApplyLoanComponent },
