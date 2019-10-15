c6c86cc31c7b32cb75ce9ab960c997427aa18652
sachin0110 <sachinhatvalne01@gmail.com>
2019-10-15 08:19:00 +0000

    working routes with left navigations

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { MainNavComponent } from './components/main-nav/main-nav.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
+// import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoanStatusComponent } from './components/loan-status/loan-status.component';
+import { ApplyLoanComponent } from './components/apply-loan/apply-loan.component';
 
 
 const routes: Routes = [
-  { path: '', component: MainNavComponent},
-  { path: 'home', component: DashboardComponent},
+  // { path: '', component: MainNavComponent },
+  { path: 'app-apply-loan', component: ApplyLoanComponent },
+  { path: 'app-loan-status', component: LoanStatusComponent },
 ];
 
 @NgModule({
