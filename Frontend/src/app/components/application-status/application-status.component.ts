0af611afa81ae13fcd4d6df087a9b731d4baa86f
sachin0110 <sachinhatvalne01@gmail.com>
2019-10-15 07:40:29 +0000

    adding components

---
+++ Frontend/src/app/components/application-status/application-status.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-application-status',
+  templateUrl: './application-status.component.html',
+  styleUrls: ['./application-status.component.scss']
+})
+export class ApplicationStatusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ Frontend/src/app/components/apply-loan/apply-loan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-apply-loan',
+  templateUrl: './apply-loan.component.html',
+  styleUrls: ['./apply-loan.component.scss']
+})
+export class ApplyLoanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
