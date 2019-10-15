0c657de81bfd2141aea69711a395591010389a11
sachin0110 <sachinhatvalne01@gmail.com>
2019-10-15 07:46:36 +0000

    Adding references to app.module

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoanStatusComponent } from './components/loan-status/loan-status.component';
+import { ApplyLoanComponent } from './components/apply-loan/apply-loan.component';
+import { ApplicationStatusComponent } from './components/application-status/application-status.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoanStatusComponent, ApplyLoanComponent, ApplicationStatusComponent
   ],
   imports: [
     BrowserModule,
