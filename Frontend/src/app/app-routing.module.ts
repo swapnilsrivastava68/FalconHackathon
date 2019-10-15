ce710e53ae44fb17e951cd164e0500f10ccfc4f7
Swapnil Srivastava <56465793+swapnilsrivastava68@users.noreply.github.com>
2019-10-15 09:07:26 +0000

    adding loan status route

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoanStatusComponent } from './components/loan-status/loan-status.component';
 import { ApplyLoanComponent } from './components/apply-loan/apply-loan.component';
+import { ApplicationStatusComponent } from './components/application-status/application-status.component';
 
 const routes: Routes = [
   { path: '', component: ApplyLoanComponent },
   { path: 'loan', component: ApplyLoanComponent },
-  { path: 'status', component: LoanStatusComponent },
+  { path: 'status', component: ApplicationStatusComponent },
+  { path: 'loan-status', component: LoanStatusComponent },
 ];
 
 @NgModule({
