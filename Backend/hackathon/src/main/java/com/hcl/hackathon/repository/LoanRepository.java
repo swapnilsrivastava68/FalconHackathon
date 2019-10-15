10c373b3359c8a83d92956d158d074852cd3d621
abhikumargithub <abhinay.neekhra85@gmail.com>
2019-10-15 13:51:06 +0000

    Merge Conflicts modification done

--- Backend/hackathon/src/main/java/com/hcl/hackathon/repository/LoanRepository.java
+++ Backend/hackathon/src/main/java/com/hcl/hackathon/repository/LoanRepository.java
@@ -1,7 +1,20 @@
-package com.hcl.hackathon.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hcl.hackathon.model.LoanDetail;
-public interface LoanRepository extends JpaRepository<LoanDetail, Integer> {
-}
+//package com.hcl.hackathon.repository;
+//
+//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
+//import org.springframework.data.repository.query.Param;
+//
+//import com.hcl.hackathon.model.LoanDetail;
+//
+///***
+// * 
+// * @author Abhinay (abhinay.neehra85@gmail.com)
+// *
+// */
+//
+//public interface LoanRepository extends JpaRepositoryImplementation<LoanDetail, Integer> {
+//	
+//	@Query("SELECT l.personalDetail FROM LoanDetail l WHERE l.id = :appId") 
+//	Integer getPersonalIdByloanId(@Param("appId") Integer appId);
+//
+//}
